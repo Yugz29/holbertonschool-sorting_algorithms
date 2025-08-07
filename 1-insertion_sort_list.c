370553ad685020395b0c02445f1ddebee866c2a5
Yugz29 <yannduzelier@gmail.com>
2025-08-07 15:19:31 +0200

    add: 1-insertion_sort_list, prototypes and print_list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -4,5 +4,37 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	
+	listint_t *current, *node;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+
+	while (current)
+	{
+		node = current;
+
+		while (node->prev != NULL && node->n < node->prev->n)
+		{
+			node->prev->next = node->next;
+
+			if (node->next)
+				node->next->prev = node->prev;
+
+			node->next = node->prev;
+			node->prev = node->prev->prev;
+
+			if (node->prev)
+				node->prev->next = node;
+
+			else
+				*list = node;
+
+			node->next->prev = node;
+
+			print_list(*list);
+		}
+		current = current->next;
+	}
 }
